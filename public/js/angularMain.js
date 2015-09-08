d42110d4e9c011bda4a98e391895a9f9447c8746
Dale Corns <drcorns@hotmail.com>
2015-09-08 10:54:38 -0700

    complete the mvp for the angular version

--- public/js/angularMain.js
+++ public/js/angularMain.js
@@ -9,29 +9,22 @@ angular.module('ga', [])
 .controller('gaController', function($scope, $http){
     $scope.movieList = [];
     $scope.favoritesList = [];
-    $scope.detail = {title: '', type: '', year: '', imdbid: ''};
+    $scope.detail = {Title: '', Type: '', Year: '', imdbID: ''};
+    $scope.searchterm = '';
     $scope.getResults = function(){
-      var searchTerm = document.getElementById('search-term').value;
-      ajaxGet('https://www.omdbapi.com/?s=' + searchTerm, function(err, data){
-        if(err){
-          console.error(err);
-        }
-        else{
-          movSrch.data = data.Search;
-          buildApiReturnView(movSrch.data);
-        }
-      });
+     var remoteResults = $http.get('https://www.omdbapi.com/?s=' + $scope.searchterm);
+      remoteResults.then(function(res){//success
+          $scope.movieList = res.data.Search;
+        }, function(){//error
+          console.error(res.data);
+        });
     };
     $scope.addFavorite = function(){
-      console.dir(movSrch.data[e.target.dataset.idx]);
-      ajaxPost('/favorites', movSrch.data[e.target.dataset.idx], function(err, data){
-        if(err){
-          console.error(err);
-          alert('There was a problem saving favorite');
-          return;
-        }
-        movSrch.favorites = data;
-        alert('Favorite Saved');
+      var addRemoteFavorite = $http.post('/favorites', $scope.detail);
+      addRemoteFavorite.then(function(res){//success
+        $scope.favoritesList = res.data;
+      }, function(res){//error
+        console.error(res.data);
       });
     };
     $scope.getFavorites = function(){
@@ -43,6 +36,6 @@ angular.module('ga', [])
       });
     };
     $scope.setDetails = function(e){
-      $scope.detail.title = e.target.innerText;
+      $scope.detail = JSON.parse(e.target.dataset.detail);
     };
   });
